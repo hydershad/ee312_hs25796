e83d11beb583c0c8c626ca0b3a639394ff08dadc
hydershad <hydershad@utexas.edu>
2016-10-09 23:13:12 -0500

    assignment4

--- assignment4/assign4.c
+++ assignment4/assign4.c
@@ -6,8 +6,9 @@
 int isUpperCase(char c);
 int isLowerCase(char c);
 int isAlphabet(char c);
-int valid(char c);
-int code_count(char str[], FILE *fp);
+int valid(char c);					//subfunction 1
+int code_count(char str[], FILE *fp);	//subfunction 2
+
 int main(void) {
 
 	int f_count = 0;
@@ -23,22 +24,22 @@ int main(void) {
 	int stop = 0;
 
 	while (1) {
-		printf("Counting SLOCs\nEnter file name to process: \n");
+		printf("Counting SLOCs\nEnter file name to process: \n");				//prompt user for filename
 		gets(filename, 99);
-		stop = strcmp(sentinel, filename);
-		if (stop == 0) {
+		stop = strcmp(sentinel, filename);			//check for sentinel
+		if (stop == 0) {						//if stop sentnel detected exits program
 			printf("Exiting Program...");
 			exit(EXIT_SUCCESS);
 		}
 		fp = fopen(filename, "r");
-		if (fp == 0){
+		if (fp == 0){							//if file was unable to be opened
 			printf("File %s failed to open\n\n", filename);
 		}
 		else {
 				int done = 0;
-				printf("\nFile Counting Summary Table\n\n");
+				printf("\nFile Counting Summary Table\n\n");			//print SLOC table
 				printf("Filename:   %s\n\n", filename);      //make sure to prep each source file for delimitng using #define BEGIN_FUNC as { and END_FUNC as }
-				printf("Function Name           #Lines of Code\n\n");
+				printf("Function Name          #Lines of Code\n\n");
 			while(done==0){
 
 				int found_type = 0;
@@ -66,29 +67,34 @@ int main(void) {
 						}
 						i = 0;
 						j = 0;
-						if (isAlphabet(str[i]) == 0 && (str[i] != '_')) {
-							while (isAlphabet(str[i]) == 0 && (str[i] != '_') && str[i] != 0) {
+						if (isAlphabet(str[i]) == 0 && (str[i] != '_')) {		
+							while (isAlphabet(str[i]) == 0 && (str[i] != '_') && str[i] != 0) {		//ignores spaces/extra junk
 								i++;
 							}
 						}
-						while ((isAlphabet(str[i]) || (str[i] == '_')) && str[i] != 0) {
+						while ((isAlphabet(str[i]) || (str[i] == '_')) && str[i] != 0) {			//ignores function type declaration
 							i++;
 						}
-						while (isAlphabet(str[i]) == 0 && (str[i] != '_') && str[i] != 0) {
+						while (isAlphabet(str[i]) == 0 && (str[i] != '_') && str[i] != 0) {		//ignores more junk/spaces
 							i++;
 						}
-						while (valid(str[i]) && str[i] != 0) {
+						while (valid(str[i]) && str[i] != 0) {					//gets function name while characters are valid
 							func_name[j] = str[i];
 							i++;
 							j++;
 						}
 						emptystring = strlen(func_name);
-						if (emptystring > 0) {
-							printf("%s		      ", func_name);
+						if (emptystring > 0) {							//checks to make sure function name was actually captured
+							printf("%s		       ", func_name);		//prints function name
 							f_count++;
-							c_count = code_count(str, fp);
-							total_c = total_c + c_count;
-							printf("%d\n", c_count);
+							c_count = code_count(str, fp);				//calls code count function
+							if (c_count == -1) {						//check mismatch case
+								printf("\n\nERROR:\nFunction and End Function Mismatch\n");
+							}
+							else{
+								total_c = total_c + c_count;			//adds code count from currnt function to total code count
+								printf("%d\n", c_count);				// prints lines of code for the function
+							}
 						}
 						else {
 							break; 
@@ -96,14 +102,16 @@ int main(void) {
 					}
 					else {
 						if (f_count == 0) {
-							printf("Filename %s does not contain any functions\n\n", filename);
+							printf("Filename %s does not contain any functions\n\n", filename);		//if no function names detected, print file contains no functions
 							done = 1;
 							fclose(fp);
 						}
 						else {
-							printf("\nTotal Functions: %d\n", f_count);
-							printf("Total Lines of Code: %d\n\n", total_c);
-							done = 1;
+							if (c_count != -1) {
+								printf("\nTotal Functions: %d\n", f_count);			//print out total function and code line count
+								printf("Total Lines of Code: %d\n\n", total_c);
+								done = 1;											//exit loop for current file, prompts for new file
+							}
 						}
 					}
 				}
@@ -119,16 +127,19 @@ int code_count(char str[], FILE *fp) {
 	int i = 0;
 	int c_count = 0;
 	char func_end[20] = "END_FCN";
-	while (end == 0) {
+	while (end == 0) {					
 		fgets(str, 299, fp);
 		i = 0;
 		line_end = 0;
-		pt = strstr(str, func_end);
-		if (pt != 0) {
+		pt = strstr(str, func_end);		//check every line to see if end of function
+		if (pt == 0 && feof(fp)) {	//check to see if END_FCN not encountered before eof for mismatch error
+			return -1;
+		}
+		if (pt != 0) {		//end code count if END_FCN encountered
 			end = 1;
 		}
 		else {
-			while (line_end == 0){
+			while (line_end == 0){				//find every semicolon not in comments or in quotes for strings
 			if (str[i] == '/') {
 				if (str[i + 1] == '*') {
 					i = i + 2;
@@ -186,9 +197,8 @@ int code_count(char str[], FILE *fp) {
 		}
 		}
 	}
-	return c_count;
+	return c_count;							//return code line count
 }
-
 
 int valid(char c) {
 	if ((48 <=c) && (c <= 57)) {
@@ -239,110 +249,3 @@ int isLowerCase(char c) {
 int isAlphabet(char c) {
 	return isUpperCase(c) || isLowerCase(c);
 }
-/*
-while (found_type == 0) {
-	if (str[i] == '/') {
-		i++;
-		if (str[i] == '*') {
-			i++;
-			while (flag == 0) {
-				switch (str[i]) {
-				case 0:
-					fgets(str, 199, fp);
-					i = 0;
-					if (feof(fp)) {
-						flag = 1;
-					}
-					break;
-				case '*':
-					if (str[i + 1] == '/') {
-						i = i + 2;
-						flag = 1;
-					}
-					else {
-						i++;
-					}
-					break;
-				default:
-					i++;
-					break;
-				}
-			}
-		}
-		if (str[i] == '/') {
-			fgets(str, 199, fp);
-			i = 0;
-		}
-	}
-	if (str[i] == '_' || (isAlphabet(str[i])) == 1) {
-		while (str[i] == '_' || (isAlphabet(str[i])) == 1) {
-			i++;
-		}
-		found_type = 1;
-	}
-	if (str[i] == 0) {
-		fgets(str, 199, fp);
-		i = 0;
-		if (feof(fp)) {
-			eof_detect = 1;
-		}
-	}
-	else {
-		i++;
-	}
-}
-flag = 0;
-while (found_func == 0) {
-	if (str[i] == '/') {
-		i++;
-		if (str[i] == '*') {
-			i++;
-			while (flag == 0) {
-				switch (str[i]) {
-				case 0:
-					fgets(str, 199, fp);
-					i = 0;
-					if (feof(fp)) {
-						flag = 1;
-					}
-					break;
-				case '*':
-					if (str[i + 1] == '/') {
-						i = i + 2;
-						flag = 1;
-					}
-					else {
-						i++;
-					}
-					break;
-				default:
-					i++;
-					break;
-				}
-			}
-		}
-
-		if (str[i] == '/') {
-			fgets(str, 199, fp);
-			i = 0;
-		}
-	}
-	if (str[i] == '_' || (isAlphabet(str[i])) == 1) {
-		while (str[i] == '_' || (isAlphabet(str[i])) == 1) {
-			func_name[j] = str[i];
-			i++;
-			j++;
-		}
-		found_func = 1;
-	}
-	if (str[i] == 0) {
-		fgets(str, 199, fp);
-		i = 0;
-		if (feof(fp)) {
-			eof_detect = 1;
-		}
-	}
-	else {
-		i++;
-	}
-}*/

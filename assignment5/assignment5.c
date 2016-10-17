2c9ac11a75fdfdaa94fa6d202ec6061c53214e13
hydershad <hydershad@utexas.edu>
2016-10-17 00:45:11 -0500

    assignment5

--- assignment5/assignment5.c
+++ assignment5/assignment5.c
@@ -32,43 +32,60 @@ int isAlphabet(char c);
 int valid(char c);
 void spellCheck(char article[], char dictionary[]) {
 	char word[50] = { 0 };
-	int word_index = 0;		//indexes for arrays
+	char printword[50] = { 0 };		//duplicate array to print original word if not in dictionary
+	int word_index = 0;				//indexes for arrays
 	int dict_index = 0;
 	int art_index = 0;
 	int done = 0;
 	int i = 0;
 	int match = 0;
 	int match_count = 0;
+
 	//how the fuck do i track outputs?????**************************
-	//	probs just make linked list/ sequential(array) through memory allocation on heap, read up on this during bus trip
+	//probs just make linked list/ sequential(array) through memory allocation on heap, read up on this during bus trip
 	
 	while (1) {
 		while ((isAlphabet(article[art_index]) && isAlphabet(article[art_index+1])) == 0 && (article[art_index] != 0)) { //will break if two letters found sequentially or if end or artile detected
 			art_index++;
 		}
-		if (article[art_index] == 0) {		//see if end of article reached (null value)
+		if (article[art_index] == 0) {																					 //see if end of article reached (null value)
 			getchar();
 			return;
 		}
 		word_index = 0;
 		for (i = 0;  i< 50; i++){
 			word[i] = 0;
+			printword[i] = 0;
 		}
 		while ((valid(article[art_index]) == 1)) {				//store characters from word into array for dict. comparison process
 			word[word_index] = ASCII2Int(article[art_index]);	//convert letters to alphabet indexes for non-case sensitive comparison with dictionary
+			printword[word_index] = article[art_index];
 			word_index++;
 			art_index++;
 		}														//break when no more alpha characters
-		word[word_index] = '\n';								//stick newline at end of word to make dictionary comparison easier to match up and to print result if not in dictionary
-		match_count = word_index;
+		word[word_index] = '\n';								//stick newline at end of word to make dictionary comparison easier to match up and to print result if not in dictionar
 		word_index = 0;
 		dict_index = 0;
-		while ((dict_index != 0) && (match == 0)) {
-			
-			if (ASCII2Int(dictionary[dict_index])!= word[word_index]){
+		match = 0;
+		while ((dictionary[dict_index] != 0) && (match == 0)) {
+			if (ASCII2Int(dictionary[dict_index])!= word[word_index]){		//skips to next word if mismatch
 				for (; dictionary[dict_index] != '\n'; dict_index++) {}
 				dict_index++;
 			}
+			if (ASCII2Int(dictionary[dict_index]) == word[word_index]) {
+				while (ASCII2Int(dictionary[dict_index]) == word[word_index]) {
+					dict_index++;
+					word_index++;
+				}
+				if ((dictionary[dict_index] == '\n') && (word[word_index] == '\n')) {
+					match = 1;
+				}
+				else { word_index = 0; }
+			}
+		}
+		if ((dictionary[dict_index] == 0) && match == 0) {
+			printf("%s", printword);
+			printf("\n");
 		}
 	}
 	getchar();
@@ -77,10 +94,10 @@ int valid(char c) {
 	if (isAlphabet(c) == 1) {
 		return 1;
 	}
-	if (c == 0x27) {			//apostrophe (')
+	if (c == 0x27) {		//apostrophe (')
 		return 1;
 	}
-	if (c == 0x2D) {
+	if (c == 0x2D) {		//hyphen
 		return 1;
 	}
 	else {
@@ -110,8 +127,14 @@ int ASCII2Int(char c) {
 	if (isUpperCase(c)) {
 		return c - 'A';
 	}
-	else if (isLowerCase(c)) {
+	if (isLowerCase(c)) {
 		return c - 'a';
+	}
+	if (c == 0x2D) {
+		return 0x2D;
+	}
+	if (c == 0x27) {
+		return 0x27;
 	}
 	else {
 		return -1;

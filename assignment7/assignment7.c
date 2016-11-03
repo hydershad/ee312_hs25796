6ba3e99fdb69ac059777c6298cd62a9a2cc28d02
hydershad <hydershad@utexas.edu>
2016-11-02 20:11:41 -0500

    assignment7

---
+++ assignment7/assignment7.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+void generateAllMatchedParens(int n){
+	int left = 0;
+	int right = 0;
+	int alt = 0;
+	int i = 0;
+	int j = n;
+
+	if(n==0){
+		return;
+	}
+	else {
+		for (i = 0; i < n; i++) {
+			for (left = 1; left < j; left++) {
+				printf("(");
+			}
+			for (alt = i; alt >=0 ; alt--) {
+				printf("()");
+			}
+			for (right = 1; right < j; right++) {
+				printf(")");
+			}
+			j--;
+			printf("\n");
+		}
+
+		return;
+	}
+	
+	//Function needs to generate all matched parens as described in the assignment
+}
+
+void generateAllPalindromicDecompositions(char str[]){
+	
+}
